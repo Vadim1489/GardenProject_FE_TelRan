b918a66fea422346a84715b6d23992df12164a2c
plyasovskikh <plyasovskikhyana1@gmail.com>
2024-10-10 20:13:21 +0400

    add logo in header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import NavMenu from '../NavMenu'
-import { Link } from 'react-router-dom'
-import s from './index.module.css'
+import NavMenu from '../NavMenu';
+import { Link } from 'react-router-dom';
+import s from './index.module.css';
+import logo from './media/logo.png';
+
+
 
 export default function Header() {
   return (
     <header className={s.header}>
-        <Link to='/'>LOGO</Link>
+        <Link to='/'> <img src={logo} alt="Logo" /></Link>
         <NavMenu />
     </header>
   )
