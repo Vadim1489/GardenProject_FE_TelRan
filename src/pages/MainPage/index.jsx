e5108d86f0b7c3919a85b0ea3102df97b4fc8958
Vadim Kiseolar <kiseolar1998@gmail.com>
2024-10-13 15:08:09 +0200

    added form with react-hook-form

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,15 +1,13 @@
 import Footer from "../../components/Footer";
 import Form from "../../components/Form";
-import SalesBlock_MainPage from "../../components/SalesBlock_MainPage";
-import Wrapper from "../../components/Wrapper";
+
+
 
 
 
 export default function MainPage() {
   return (
     <div>
-      <Wrapper />
-      <SalesBlock_MainPage />
       <Form />
       <Footer/>
     </div>
