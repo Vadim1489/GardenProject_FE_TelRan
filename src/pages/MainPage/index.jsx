7a70eb6f080f81602832b4f0d2d5b887b6320815
Vadim Kiseolar <kiseolar1998@gmail.com>
2024-10-13 23:07:53 +0200

    added wrapper

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,11 +1,14 @@
 import Footer from "../../components/Footer";
 import SalesBlockMailPage from "../../components/SalesBlockMainPage"; // Выбор первого варианта
 import Form from "../../components/Form"; // Выбор второго варианта
+import WrapperContainer from "../../components/WrapperContainer";
+import Header from "../../components/Header";
 
 export default function MainPage() {
   return (
     <div>
-      
+      <Header />
+      <WrapperContainer />
       <Form />
       <SalesBlockMailPage /> 
       <Footer/>
