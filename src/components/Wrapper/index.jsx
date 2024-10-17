9d8ea50a116d12bf50a97a2c2ec0212a3d125fc0
Vadim Kiseolar <kiseolar1998@gmail.com>
2024-10-17 11:12:47 +0200

    updated wrapper

--- src/components/Wrapper/index.jsx
+++ src/components/Wrapper/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import s from './index.module.css'
+import { Link } from 'react-router-dom'
 
 export default function Wrapper() {
   return (
     <div className={s.wrapper}>
         <h1>Amazing Discounts <br/> on Garden Products!</h1>
-        <div className={s.btn}>Check out</div>
+        <Link to={'/all_sales'}>
+          <div className={s.btn}>Check out</div>
+        </Link>
     </div>
   )
 }
