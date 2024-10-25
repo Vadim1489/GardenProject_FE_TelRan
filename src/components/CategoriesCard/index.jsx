012a05fe6fc45d01cbefa12f985f621bf23faeac
plyasovskikh <plyasovskikhyana1@gmail.com>
2024-10-25 18:33:47 +0400

    update style and fix a bug

--- src/components/CategoriesCard/index.jsx
+++ src/components/CategoriesCard/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 
 export default function CategoriesCard({ title, image, id }) {
   return (
-    <Link to={`/categories/${id}`} className={s.categories-card}>
+    <Link to={`/categories/${id}`} className={s.categories_card}>
       <img src={`${domen}${image}`} alt={title} />
       <h5>{title}</h5>
     </Link>
